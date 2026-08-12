075cf348819996ef8072f0af8345702abb50b8b8
mavischx <mavis.chx@gmail.com>
2026-08-12 10:44:08 +0100

    match service skeleton

---
+++ matchservice/src/main/java/com/example/matchservice/MatchserviceApplication.java
@@ -0,0 +1,15 @@
+package com.example.matchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchserviceApplication.class, args);
+
+        System.out.println("Hello match");
+    }
+
+}
