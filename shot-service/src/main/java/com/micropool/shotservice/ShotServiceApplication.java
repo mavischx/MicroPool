1b99484f0ff822d3b64891f0249ccbeaa9f664f1
RobertasBr <s00238718@atu.ie>
2026-08-12 10:49:14 +0100

    Rack 1 commit

---
+++ shot-service/src/main/java/com/micropool/shotservice/ShotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.micropool.shotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShotServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShotServiceApplication.class, args);
+    }
+
+}
