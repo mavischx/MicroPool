55f4265e91d7e5c2ccd884b0204514f5129b0b8f
RobertasBr <s00238718@atu.ie>
2026-08-13 09:57:19 +0100

    shot service boiletplate added

--- shot-service/src/test/java/com/micropool/shotservice/ShotServiceApplicationTests.java
+++ shot-service/src/test/java/com/micropool/shotservice/ShotServiceApplicationTests.java
@@ -1,6 +1,12 @@
+package com.micropool.shotservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
 @SpringBootTest
 class ShotServiceApplicationTests {
 
     @Test
     void contextLoads() {
     }
+}
